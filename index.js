25eade69155a8a609eb8bb2b7c1c471c9968566d
Maria Wieckowska <mkw98@gazeta.pl>
2018-09-23 19:15:21 +0200

    edit index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var os = require('os');
 var OSinfo = require('./modules/OSinfo');
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.')
+});
+emitter.on('afterCommand', function() {
+console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 
@@ -7,6 +16,7 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
+        emitter.emit('beforeCommand', instruction);
         switch (instruction) {
         	case '/exit':
             process.stdout.write('Quitting app!\n');
@@ -27,5 +37,6 @@ process.stdin.on('readable', function() {
         	default:
             process.stderr.write('Wrong instruction!\n');
         }
+        emitter.emit('afterCommand');
     }
 });
